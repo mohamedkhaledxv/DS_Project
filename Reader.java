630dbe890373f31f51d4fa8582dfa4975f0b753d
Mohamed Khaled <105593823+mohamedkhaledxv@users.noreply.github.com>
2022-12-16 18:52:05 +0200

    Create Reader.java

---
+++ Reader.java
@@ -0,0 +1,57 @@
+
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+  
+    static String  filepath;
+  
+    public static String Read( )
+    {
+
+        FileChooser fc = new FileChooser();
+        File selectedFile = fc.showOpenDialog(null);
+        fc.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("XML files","*.png")); 
+        String s = "";
+        if(selectedFile!=null)
+        {
+
+            FileReader fr = null ;
+            try {
+                fr = new FileReader(selectedFile.getAbsolutePath());
+                filepath =selectedFile.getAbsolutePath();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+            int i = 0;
+            int counter = 0;
+            while(true)
+            {
+                try {
+                    if (!((i=fr.read()) != -1))
+                    {
+                        counter--;
+                        break; 
+                    }
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                counter++;
+                //System.out.print((char)i);
+
+                s+=(char)i;
+            }
+            System.out.println(counter);
+
+
+        }
+        else
+            System.out.println("File is not valid");
+        return s;
+    }
+}
