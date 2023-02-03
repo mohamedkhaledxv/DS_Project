84ccfd4fff2bc2c1310196c0f24f3c71162bdc50
Mohamed Khaled <105593823+mohamedkhaledxv@users.noreply.github.com>
2023-02-03 13:36:27 +0200

    Update Main.java

--- Project/Main.java
+++ Project/Main.java
@@ -1,3 +1,5 @@
+package com.example.guil;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,17 +11,18 @@ public class Main extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 240);
         stage.setTitle("XML Editor");
         stage.setScene(scene);
-        stage.setHeight(600);
-        stage.setWidth(460);
-        stage.setResizable(false);
+        stage.setHeight(700);
+        stage.setWidth(520);
+        stage.setResizable(true);
         stage.show();
 
     }
 
     public static void main(String[] args) {
-        launch(args);
+
+        Main.launch(args);
     }
 }
